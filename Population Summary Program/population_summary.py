92215d432206f5e13eab6b0ae1d24ba7c1725077
IncomingWill <thewilliamedwards@gmail.com>
2022-02-01 19:52:05 -0600

    Add project files.

---
+++ Population Summary Program/population_summary.py
@@ -0,0 +1,8 @@
+# Title: Population Summary Program
+# Program created by William Schaeffer
+# CPS 313
+# P. 428, Exercise 9, Population Summary Program
+# 02.01.22
+
+# This program will take steps recorded in steps.txt and display average number of steps each month
+
